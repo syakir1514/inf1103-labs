060b02676ba00212973dba5e9a39a7b1c58e95e2
Syakir <syakir1514@gmail.com>
2026-09-04 11:51:31 +0800

    activity 5, in A4

--- A4.py
+++ A4.py
@@ -1,5 +1,5 @@
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -7,3 +7,6 @@ print("================")
 print("Username:", username)
 print("Age:", age)
 print("Content Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
