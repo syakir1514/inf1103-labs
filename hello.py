9654b0c5bfb5f5e757f77be20ad2fb10c5ff5ba8
Syakir <syakir1514@gmail.com>
2026-09-04 11:22:54 +0800

    added username, bio, followers

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("===============")
 print("Welcome Here")
 print("My first post!")
 print("===============")
+
+user_name = ("ExaShark")
+bio = ("Filmmaker + Engineer")
+followers = 1000
+
+print("User Name:", user_name)
+print("Bio:", bio)
+print("Followers:", followers)
+
