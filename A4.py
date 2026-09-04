eef3a7333077ffd6a0f5416605b2740c257c6af2
Syakir <syakir1514@gmail.com>
2026-09-04 11:37:44 +0800

    new file, A4.py for activity 4

---
+++ A4.py
@@ -0,0 +1,9 @@
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("================")
+print("Username:", username)
+print("Age:", age)
+print("Content Category:", category)
