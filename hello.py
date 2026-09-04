08e48129dba5c63ccff88d77f5407e9bd8293af0
Syakir <syakir1514@gmail.com>
2026-09-04 11:29:37 +0800

    day 1-3 follower updates

--- hello.py
+++ hello.py
@@ -11,3 +11,13 @@ print("User Name:", user_name)
 print("Bio:", bio)
 print("Followers:", followers)
 
+followers = 100
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
+
